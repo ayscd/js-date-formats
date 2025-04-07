0da2280ebd39c2a8a73ba998938f66c3a9812528
ayscd <ayeskacsta@gmail.com>
2025-04-07 18:31:30 -0300

    Update 2 - future date
    - displaying future date using moment library

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+function displayFutureDate() {
+    let day = moment().add(10, "years").format("dddd");
+    let month = moment().add(10, "years").format("MMMM");
+    let date = moment().add(10, "years").format("D");
+    let year = moment().add(10, "years").format("YYYY");
+    let hours = moment().add(10, "years").format("HH");
+    let minutes = moment().add(10, "years").format("mm");
+    let seconds = moment().add(10, "years").format("ss");
+
+
+    let futureDate = document.querySelector("#future-date");
+    futureDate.innerHTML = `10 years from now, it will be ${day}, ${month} ${date}, ${year} - ${hours}:${minutes}:${seconds}`;
+}
+
+
 function displayDate() {
     let day = moment().format("dddd");
     let month = moment().format("MMMM");
@@ -9,3 +24,4 @@ function displayDate() {
 }
 
 displayDate();
+displayFutureDate();
